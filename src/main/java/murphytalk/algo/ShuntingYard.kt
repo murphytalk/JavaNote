d0b7f5784cb5aa78882af9785071fdb613737b68
murphytalk <murphytalk@gmail.com>
2017-05-28 21:52:33 +0900

    Shunting yard algorithm to evaluate agrithmetic

--- src/main/java/murphytalk/algo/ShuntingYard.kt
+++ src/main/java/murphytalk/algo/ShuntingYard.kt
@@ -1,7 +1,6 @@
 package murphytalk.algo
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * Created by murphytalk on 5/22/2017.
@@ -12,21 +11,20 @@ fun RPN(s: String): Int {
     val num = StringBuilder()
     for (c in s) {
         if (c == ' ') {
-            stack.push(Integer.parseInt(num.toString()))
+            stack.push(num.toString().toInt())
             num.delete(0, num.length)
         } else {
-            val v = c.toInt() - '0'.toInt()
-            if (v in 0..9) {
-                num.append(v)
+            if(c in '0'..'9'){
+                num.append(c)
             } else {
-                //the 1st value could still be in buffer num
+                //the 2nd value could still be in buffer num
                 val v2 = if (num.length > 0) num.toString().toInt() else stack.pop()
                 num.delete(0, num.length)
-                //pop the 2nd value
+                //pop the 1st value
                 val v1 = stack.pop()
                 stack.push(
                         when (c) {
-                            'x', '*' -> v1 * v2
+                            '*' -> v1 * v2
                             '/' -> v1 / v2
                             '-' -> v1 - v2
                             '+' -> v1 + v2
@@ -41,20 +39,81 @@ fun RPN(s: String): Int {
 }
 
 // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+// todo: support function
 fun shuntingYard(s: String): String {
+    /**
+     * Compare operator order/precedence
+     * Return value :
+     *     >0 => o1 > o2
+     *    ==0 => o1 = o2
+     *     <0 =? o1 < o2
+     */
+    fun operatorOrder(o1: Char, o2: Char): Int {
+        fun conv(o: Char): Int {
+            return if (o == '+' || o == '-') 0
+                   else if (o == '*' || o == '/') 1
+                   else 2 //^
+        }
+        return conv(o1) - conv(o2)
+    }
+
+    //0: left, 1: right, 2: non
+    fun associativity(o: Char): Int{
+        return when(o){
+            '+','-','*','/' -> 0
+            '^' -> 1
+            else -> 2
+        }
+    }
+
     val stack = Stack<String>()
     val output = StringBuilder()
     for (c in s) {
-        if ((c.toInt() - '0'.toInt()) in 0..9) {
+        if (c in '0'..'9') {
             output.append(c)
         } else {
-            /*
-            when(c){
-                'x','*','/','+','-' ->
-
+            when (c) {
+                '*', '/', '+', '-', '^' -> {
+                    if (stack.size >= 1) {
+                        val o2 = stack.peek()[0]
+                        val order = operatorOrder(c /*o1*/, o2)
+                        val ass = associativity(o2)
+                        if( ((ass == 0 /*left-associative*/ ) && (order <= 0)) ||
+                            ((ass == 1 /*right-associative*/) && (order <0))) {
+                                stack.pop()
+                                output.append(o2)
+                        }
+                    }
+                    if(output.last() in '0'..'9') {
+                        output.append(" ")/*number separator*/
+                    }
+                    stack.push(c.toString())
+                }
+                '(' -> stack.push(c.toString())
+                ')' -> {
+                    var foundLeftParenthesis = false
+                    do {
+                        if (stack.size == 0)
+                            throw RuntimeException("Mismatched parenthesis")
+                        else {
+                            val c2 = stack.pop()
+                            if (c2[0] == '(')
+                                foundLeftParenthesis = true
+                            else
+                                output.append(c2)
+                        }
+                    } while (!foundLeftParenthesis)
+                }
+                else -> throw RuntimeException("Invalid operator $c")  //todo: support function
             }
-            */
         }
+    }
+    while (stack.size > 0) {
+        val c2 = stack.pop()
+        if (c2[0] == '(' || c2[0] == ')')
+            throw RuntimeException("Mismatched parenthesis")
+        else
+            output.append(c2)
     }
     return output.toString()
 }
--- src/test/java/murphytalk/algo/TestShuntingYard.kt
+++ src/test/java/murphytalk/algo/TestShuntingYard.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 /**
  * Created by murphytalk on 5/22/2017.
  */
-class TestShuntingYard{
+class TestShuntingYard {
     @Test fun testRPN1() {
         assertThat(RPN("5 1 2+4*+3-"), `is`(14))
     }
@@ -16,11 +16,27 @@ class TestShuntingYard{
         assertThat(RPN("25 50+"), `is`(75))
     }
 
-    @Test fun testRPN3(){
+    @Test fun testRPN3() {
         assertThat(RPN("4 5^"), `is`(1024))
     }
 
-    @Test(expected=RuntimeException::class) fun testRPN_InvalidOperator(){
+    @Test(expected = RuntimeException::class) fun testRPN_InvalidOperator() {
         RPN("512D34")
+    }
+
+    @Test fun shuntingYard() {
+        assertThat(shuntingYard("3+4*2/(1-5)^2^3"), `is`("3 4 2*1 5-2 3^^/+"))
+    }
+
+    @Test fun eval1(){
+        assertThat(evalArithmetic("5+((1+2)*4)-3"),`is`(14))
+    }
+
+    @Test fun eval2(){
+        assertThat(evalArithmetic("25+50"),`is`(75))
+    }
+
+    @Test fun eval3(){
+        assertThat(evalArithmetic("4^5"),`is`(1024))
     }
 }
