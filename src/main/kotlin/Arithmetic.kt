c7b48af5d831fa1091ba24b617d68716a1e11608
murphytalk <murphytalk@gmail.com>
2017-05-31 21:37:37 +0900

    genearate elementary arithmetic

--- src/main/kotlin/Arithmetic.kt
+++ src/main/kotlin/Arithmetic.kt
@@ -3,17 +3,29 @@ import org.fusesource.jansi.Ansi.Color.*
 import org.fusesource.jansi.Ansi.ansi
 import org.fusesource.jansi.AnsiConsole
 
-class Arithmetic{
-    fun generate():String {
-        return ""
-    }
+class Arithmetic(generator:ArithmeticGenerator, num:Int){
+    // array of generated (question, answer) pairs
+    val questions = Array(num, { _ -> val s = generator.generate(); Pair(s, evalArithmetic(s)) })
+    // to save answers answered by user
+    val answers = Array(num, { _ -> 0 })
 }
 
 fun main(args:Array<String>){
-    AnsiConsole.systemInstall();
+    AnsiConsole.systemInstall()
 
-    println(ansi().eraseLine().fg(RED).a("TEST ").fg(GREEN).a(" BEGIN!"))
+    println(ansi().eraseScreen().fg(GREEN).a("Hi buddy, let's do some math!"))
+    println()
 
-    println(ansi().fg(WHITE).a("END"))
-    AnsiConsole.systemUninstall();
+    val b = ArithmeticGenerator.Builder()
+    val generator = b.build()
+
+    val arithmetic = Arithmetic(generator, 10)
+
+    arithmetic.questions.map { (question, answer) ->  println(ansi().fg(YELLOW).a(question).fg(WHITE).a(" = ").fg(BLUE).a(answer)) }
+
+
+    println()
+    println(ansi().fg(WHITE).a("See you next time !"))
+
+    AnsiConsole.systemUninstall()
 }
---
+++ src/main/kotlin/ArithmeticGenerator.kt
@@ -0,0 +1,87 @@
+package murphytalk.arithmetic
+
+import java.util.*
+
+/**
+ * Created by murphytalk on 5/31/2017.
+ */
+class ArithmeticGenerator private constructor(val operatorNum:Int, val noNageative: Boolean,
+                                              val allowMultiplication: Boolean, val allowDivision: Boolean,
+                                              val numberDigitsPossibilities: Array<Pair<Int,Int>>){
+
+    private val random = Random()
+
+    private fun generateOperator(): String {
+        //division can only be allowed when multiplication is allowed
+        val possibleOperators =  if(allowMultiplication) {if(allowDivision) arrayOf('+','-','*','/') else arrayOf('+','-','*')} else arrayOf('+','-')
+        return possibleOperators[random.nextInt(possibleOperators.size)].toString()
+    }
+
+    private fun generateNumber(): Int {
+        val p = random.nextInt(101) // an integer within [0, 100]
+        var v = 0
+        for ((possibility, digits) in numberDigitsPossibilities){
+            if(p<=possibility){
+                do {
+                    v = Math.floor(random.nextFloat() * Math.pow(10.0, digits.toDouble())).toInt()
+                } while(v < 100 ) //at least 2 digits
+                break
+            }
+        }
+        return v
+    }
+
+    fun generate(): String{
+        val sb = StringBuilder()
+        var numOfOperator = 0
+
+        sb.append(generateNumber())
+        while(numOfOperator<operatorNum){
+            sb.append(generateOperator()).append(generateNumber())
+            ++numOfOperator
+        }
+
+        return sb.toString()
+    }
+
+    class Builder{
+        private var operatorNum:Int = 3
+        private var noNageative: Boolean = true
+        private var allowMultiplication: Boolean = true
+        private var allowDivision: Boolean = false
+        //default : 80% chance to generate a 3 digits number; 20% chance to generate a 4 digits number
+        //needs to be sorted by chance in descending order
+        private var numberDigitsPossibilities: Array<Pair<Int,Int>> = arrayOf(Pair(80,3), Pair(20,4))
+
+        fun setOperatorNum(n:Int?): Builder{
+            if(n!=null) operatorNum = n
+            return this
+        }
+
+        fun setNoNegative(b:Boolean?): Builder{
+            if(b!=null) noNageative = b
+            return this
+        }
+
+        fun setAllowMultiplication(b:Boolean?): Builder{
+            if(b!=null) allowMultiplication = b
+            return this
+        }
+
+        fun setAllowDivision(b:Boolean?): Builder{ //division can only be allowed when multiplication is allowed
+            if(allowMultiplication) {
+                if (b != null) allowDivision = b
+            }
+            return this
+        }
+
+        fun setNumberDigitsPossibilities(p: Array<Pair<Int,Int>>?):Builder{
+            if(p!=null) numberDigitsPossibilities = p
+            return this
+        }
+
+        fun build(): ArithmeticGenerator{
+            return ArithmeticGenerator(operatorNum, noNageative, allowMultiplication, allowDivision, numberDigitsPossibilities)
+        }
+    }
+}
