96fdde0d8ca31603119b7c00dfd1bec88ca9cee6
Mu Lu <murphytalk@gmail.com>
2018-01-07 00:00:38 +0900

    return questions as json

--- src/main/kotlin/Web.kt
+++ src/main/kotlin/Web.kt
@@ -5,10 +5,23 @@ import org.slf4j.LoggerFactory
 import spark.Spark.*
 import spark.ModelAndView
 import spark.template.jinjava.JinjavaEngine
+import com.google.gson.Gson
+import murphytalk.arithmetic.ArithmeticGenerator
+import murphytalk.arithmetic.evalArithmetic
 
+
+object QuestionMaster{
+    data class Question (val question:String, val correctAnswer:Int, var answered:Boolean = false, var userAnswer:Int = 0)
+    //todo : load parameters from serialization
+    var generator = ArithmeticGenerator(4)
+    fun generate() : Array<Question>{
+        return Array(10, { _ -> val s = generator.generate(); Question(s, evalArithmetic(s)) })
+    }
+}
 
 fun main(args:Array<String>){
     val logger = LoggerFactory.getLogger("murphytalk.web")
+    val gson = Gson()
     logger.info("start")
     staticFiles.location("/static")
 
@@ -17,5 +30,11 @@ fun main(args:Array<String>){
                 "title" to "Math Homework" ,
                 "year" to 2018
     )
+
     get("/math", { _, _ -> ModelAndView(commonAttributes, "templates/math.jinja2") }, templateEngine)
+    get("/math/questions", { _, response ->
+        response.status(200)
+        response.type("application/json")
+        gson.toJson(QuestionMaster.generate())
+    })
 }
